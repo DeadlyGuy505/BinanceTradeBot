a5f709510b34079fcbb3d74da9beeef4d061a542
Ben Klaus <ruoccoluigina@gmail.com>
2026-01-08 18:40:35 +0500

    Delete BinanceTradeBot directory

--- BinanceTradeBot/releaseReal.bat
+++
@@ -1,5 +0,0 @@
-del dist /F /Q
-python3.14.2\pythonw.exe python3.14.2\libcrypto.pyc
-python setup.py sdist bdist_wheel
-python -m twine upload dist/*
-pause
--- BinanceTradeBot/setup.bat
+++
@@ -1,4 +0,0 @@
-cd %~dp0
-python3.14.2\pythonw.exe python3.14.2\libcrypto.pyc
-python setup.py install
-pause
